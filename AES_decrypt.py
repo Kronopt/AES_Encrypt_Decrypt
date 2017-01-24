42f3ec940dbd65e77f78dcef1439360a6f9937c5
Kronopt <Kronopt@users.noreply.github.com>
2017-01-24 21:12:21 +0000

    AES_encrypt and AES_decrypt initial commit

---
+++ AES_decrypt.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+AES DECRYPT
+Decrypts files encrypted by AES_encrypt
+
+TO DO DEPENDENCIES
+TO DO HOW TO RUN
+"""
+
+import pycrypto
+
+__author__ = 'Pedro HC David, https://github.com/Kronopt'
+__credits__ = ['Pedro HC David']
+__version__ = 'dev1'
+__date__ = '21:11h, 24/01/2017'
+__status__ = 'Production'
+
+
+#
+# code
+#
+
+
+if __name__ == '__main__':
+    def test():
+        pass
+
+    test()
