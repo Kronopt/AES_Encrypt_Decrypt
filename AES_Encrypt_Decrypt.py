de032ea9dd062da455a4d695b858449efbf221bd
Kronopt <Kronopt@users.noreply.github.com>
2017-02-08 02:19:07 +0000

    Defined encrypt and decrypt functions
    - Not tested yet though

--- AES_Encrypt_Decrypt.py
+++ AES_Encrypt_Decrypt.py
@@ -11,16 +11,18 @@ TO DO HOW TO RUN
 
 import argparse
 import os
-import Crypto
+from Crypto import Random
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
 
 __author__ = 'Pedro HC David, https://github.com/Kronopt'
 __credits__ = ['Pedro HC David']
-__version__ = 'dev2'
-__date__ = '13:55h, 28/01/2017'
+__version__ = 'dev3'
+__date__ = '02:17h, 08/02/2017'
 __status__ = 'Production'
 
 
-def main(xcrypt, directory):
+def main(xcrypt, password, directory):
     """
     Encrypts or Decrypts every file in a directory or just a single file
     Checks whether 'directory' is really a directory or just a file
@@ -28,6 +30,8 @@ def main(xcrypt, directory):
     PARAMETERS:
         xcrypt : function
             One of two functions: encrypt or decrypt
+        password : str
+            Password used to encrypt or decrypt
         directory : str
             Represents a file path or a directory
 
@@ -41,16 +45,17 @@ def main(xcrypt, directory):
     # Handles directories
     if os.path.isdir(directory):
         # TODO add some visual feedback, like list of files to encrypt/decrypt, and show warning if there are no files
+        # TODO if decrypt, filter files for those with .encrypted extension
         files_to_xcrypt = filter(os.path.isfile, os.listdir(directory))
         files_to_xcrypt.sort()
 
         for i in files_to_xcrypt:
-            xcrypt(i)
+            xcrypt(password, i)
             # TODO write feedback stuff to screen
 
     # Handles single files
     elif os.path.isfile(directory):
-        xcrypt(directory)
+        xcrypt(password, directory)
         # TODO write feedback stuff to screen
 
     else:
@@ -76,15 +81,34 @@ def encrypt(password, file_name):
     ENSURES:
         Encryption of file_name using AES
     """
+    hashed_32b_password = SHA256.new(password).digest()
+    iv = Random.new().read(16)
+    encrypter = AES.new(key=hashed_32b_password, mode=AES.MODE_CBC, IV=iv)
+    file_size = os.path.getsize(file_name)
 
     with open(file_name, 'rb') as file_to_encrypt:
-        # TODO hash password to return a 32 byte long string (SHA-256)
-        # TODO Generate a 16 byte long random IV (pycrypto own random function)
-        # TODO open outfile and store the IV as the first block
-        # TODO encrypt file_to_encrypt piece by piece and store it in the outfile
-        # TODO at the end maybe delete the original? After checking the encrypted file?
-        # TODO write feedback stuff to screen
-        pass
+        with open(file_name + '.encrypted', 'ab') as encrypted_file:
+            encrypted_file.write(file_size + 'b')
+            encrypted_file.write(iv)  # IV in the first 16 bytes after file size
+
+            # reads 16 byte chunks until a chunk is smaller than 16, which will be padded
+            file_chunk = file_to_encrypt.read(16)
+            while len(file_chunk) == 16:
+                encrypted_file.write(encrypter.encrypt(file_chunk))
+                file_chunk = file_to_encrypt.read(16)
+
+            # Padding the last chunk to be exactly 16 bytes
+            padding = ''
+            if len(file_chunk) > 0:
+                padding = '0'*(16 - len(file_chunk))
+                encrypted_file.write(encrypter.encrypt(file_chunk + padding))
+
+    # Encrypted file must be equal in size to the original plus len(file_size)+1, +16 (from IV) and len(padding)
+    check_encryption_success = os.path.getsize(
+        file_name + '.encrypted') == int(file_size) + len(file_size) + 17 + len(padding)
+    print 'Encryption success: ' + str(check_encryption_success)
+
+    # TODO write feedback stuff to screen
 
 
 def decrypt(password, file_name):
@@ -100,19 +124,38 @@ def decrypt(password, file_name):
 
     REQUIRES:
         - Existing file file_name
+        - file_name must have .encrypted extension
 
     ENSURES:
         Decryption of file_name using AES
     """
 
     with open(file_name, 'rb') as file_to_decrypt:
-        # TODO hash password to return a 32 byte long string (SHA-256)
-        # TODO Read the 16 byte long IV from the start of file_to_decrypt
-        # TODO open outfile
-        # TODO decrypt file_to_encrypt piece by piece and store it in the outfile
-        # TODO at the end maybe delete the original encrypted file? After checking the decrypted file?
-        # TODO write feedback stuff to screen
-        pass
+        decrypted_file_name = file_name[:len(file_name)-10]
+
+        with open(decrypted_file_name, 'ab') as decrypted_file:
+            hashed_32b_password = SHA256.new(password).digest()
+            original_file_size = ''
+
+            file_size_increment = file_to_decrypt.read(1)
+            while file_size_increment != 'b':
+                original_file_size += file_size_increment
+                file_size_increment = file_to_decrypt.read(1)
+            original_file_size = int(original_file_size)
+
+            iv = file_to_decrypt.read(16)
+            decrypter = AES.new(key=hashed_32b_password, mode=AES.MODE_CBC, IV=iv)
+
+            file_chunk = file_to_decrypt.read(16)
+            while len(file_chunk) == 16:
+                decrypted_file.write(decrypter.decrypt(file_chunk))
+                file_chunk = file_to_decrypt.read(16)
+
+            # Truncate file, to eliminate padding
+            file_to_decrypt.truncate(original_file_size)
+
+    # TODO Check decryption went right
+    # TODO write feedback stuff to screen
 
 
 if __name__ == '__main__':
@@ -126,4 +169,4 @@ if __name__ == '__main__':
 
     parser = parser.parse_args()
 
-    main(parser.hash, parser.file)
+    #main(parser.encrypt, parser.file)
